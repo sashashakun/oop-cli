1c57ad1c7f0c71a182a3b8595aa7c10ecbf406f8
Alex Shakun <alx.safari@gmail.com>
2019-02-09 15:17:31 +0300

    mooooore classes

--- src/main.js
+++ src/main.js
@@ -10,7 +10,26 @@ export class Geomagic {
   async getLocationDataByIp(ipAddress) {
     const { data } = await this.client.get(`${geoLocationServiceUrl}${ipAddress || ''}`);
 
-    return data.city || 'No such info in our service, sorry. Try again!';
+    const geoData = new GeoApiResponse(data);
+
+    return geoData.get('city') || 'No such info in our service, sorry. Try again!';
+  }
+}
+
+class GeoApiResponse {
+
+  constructor({ city, country, regionName, timezone, zip }) {
+    this.city = city;
+    this.country = country;
+    this.regionName = regionName;
+    this.timezone = timezone;
+    this.zip = zip;
+  }
+
+  get(fieldName) {
+    if (this[fieldName]) {
+      return this[fieldName];
+    }
   }
 }
 
