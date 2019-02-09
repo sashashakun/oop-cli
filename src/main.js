bcbdbc95b817ea2765ad07b6f3eb85744012726e
Alex Shakun <alx.safari@gmail.com>
2019-02-09 09:21:26 +0300

    fix default return

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import axios from 'axios';
 const geoLocationServiceUrl = 'http://ip-api.com/json/';
 
 export const makeGeoIpLocator = httpClient => async (ipAddress) => {
-  const { 
-    data = { city: 'No such info in our service, sorry. Try again!' }
-  } = await httpClient.get(`${geoLocationServiceUrl}${ipAddress || ''}`);
+  const { data } = await httpClient.get(`${geoLocationServiceUrl}${ipAddress || ''}`);
 
-  return data.city;
+  return data.city || 'No such info in our service, sorry. Try again!';
 };
 
 export default makeGeoIpLocator(axios);
